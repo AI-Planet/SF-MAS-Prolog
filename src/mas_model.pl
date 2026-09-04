84e262bd8271536b3de8880aad2228a3cba3c453
Mehdi Jalali <119305979+AI-Planet@users.noreply.github.com>
2026-09-04 18:51:58 +0200

    Create mas_model.pl

---
+++ src/mas_model.pl
@@ -0,0 +1,46 @@
+
+% =====================================================
+% Agents
+% =====================================================
+
+agent(alice).
+agent(bob).
+agent(drone).
+
+
+% =====================================================
+% Actions
+% =====================================================
+
+action(observe).
+action(move).
+action(open_door).
+action(rescue).
+action(wait).
+
+
+% =====================================================
+% States
+% =====================================================
+
+state(s0).
+state(s1).
+state(s2).
+
+
+% =====================================================
+% Transitions
+% =====================================================
+
+transition(s0, move(alice), s1).
+
+transition(s1, open_door(alice), s2).
+
+transition(s2, rescue(alice, bob), s2).
+
+transition(s2, observe(drone), s2).
+
+transition(s0, wait(alice), s0).
+transition(s1, wait(alice), s1).
+transition(s2, wait(alice), s2).
+
